a7563350aeefa908bad921c30485715f8c7bb516
rajendrazero <122064592+rajendrazero@users.noreply.github.com>
2025-10-07 09:50:55 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,13 +2,16 @@
 
 $db   = "db_siswa";
 $host = "maglev.proxy.rlwy.net";
-$port = "35682"; // ganti sesuai port Railway DB-mu
+$port = 35682; // port sebagai integer, bukan string
 $user = "root";
 $pass = "RQDnCUbLBvpLHlbgfDDGuORKVjFOznhB";
 
-$conn = new mysqli($host, $port, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
   die("Koneksi gagal: " . $conn->connect_error);
 }
+
+echo "Koneksi berhasil!";
+
 ?>
