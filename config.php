b72eb63c458fdfb5c8bea4c67b11ea0d1f1cbeec
rajendrazero <122064592+rajendrazero@users.noreply.github.com>
2025-10-09 16:58:36 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,15 +1,16 @@
 <?php
+// Koneksi ke database Supabase PostgreSQL
+$host     = "aws-1-ap-southeast-1.pooler.supabase.com";
+$port     = "6543";
+$dbname   = "postgres";
+$user     = "postgres.dlcyjvspmnzxdryumqgk";
+$password = "rajendraathallahfawwaz";
 
-$db   = "db_siswa";
-$host = "maglev.proxy.rlwy.net";
-$port = 35682; // port sebagai integer, bukan string
-$user = "root";
-$pass = "RQDnCUbLBvpLHlbgfDDGuORKVjFOznhB";
-
-$conn = new mysqli($host, $user, $pass, $db, $port);
-
-if ($conn->connect_error) {
-  die("Koneksi gagal: " . $conn->connect_error);
+try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // echo "Koneksi berhasil";
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
 }
-
 ?>
